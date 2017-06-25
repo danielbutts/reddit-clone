6e24c5a2cbb9544d1469e91d58c7e7c9d8293bb9
danielbutts <daniel.butts@gmail.com>
2017-06-24 23:46:00 -0700

    knexfile changes for production deployment

--- app/knexfile.js
+++ app/knexfile.js
@@ -12,6 +12,13 @@ module.exports = {
     connection: {
       database: process.env.DATABASE_URL || 'reddit-clone-test',
     }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      database: process.env.DATABASE_URL,
+    }
   }
 
 };
